262175c7a81adc7dd5e425a29129a02eba67b6d8
Suraj Nagnath Sontakke <56450895+suraj1299@users.noreply.github.com>
2022-05-18 19:08:14 +0530

    Add files via upload
    file changes commited

---
+++ CustomerAPI/CustomerAPI/services/CustomerService.cs
@@ -0,0 +1,47 @@
+﻿using RestaurantAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerWebAPI.services
+{
+    public class CustomerService : ICustomerService
+    {
+        
+            public static readonly List<Customer> customers = new List<Customer>()
+            {
+               new Customer {CustomerID ="1",CustomerName="Sammy"},
+               new Customer {CustomerID ="2",CustomerName="Jake"},
+
+            };
+
+
+
+
+            public async Task<IEnumerable<Customer>> GetCustomers()
+            {
+                return customers;
+            }
+
+
+
+           /* public async Task<Customer> GetCustomerById(int CustomerID)
+            {
+                var result = customers.SingleOrDefault(a => a.CustomerID == CustomerID);
+                return result;
+            }
+
+
+
+            public async Task<Customer> CreateCustomer(Customer CustomerObj)
+            {
+                customers.Add(CustomerObj);
+                return CustomerObj;
+            }*/
+
+
+
+            
+}
+}
